24606e06c29ceae44c6ba4cbf18196c88ef99e03
Anselmo Ichihara <ichihara7l@gmail.com>
2022-05-11 07:00:57 -0300

    Adding card confirmation

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -22,26 +22,6 @@ export default function Confirmation() {
         rounded={'lg'}
         p={6}
         textAlign={'center'}>
-        <Avatar
-          size={'xl'}
-          src={
-            'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-          }
-          alt={'Avatar Alt'}
-          mb={4}
-          pos={'relative'}
-          _after={{
-            content: '""',
-            w: 4,
-            h: 4,
-            bg: 'green.300',
-            border: '2px solid white',
-            rounded: 'full',
-            pos: 'absolute',
-            bottom: 0,
-            right: 3,
-          }}
-        />
         <Heading fontSize={'2xl'} fontFamily={'body'}>
           Lindsey James
         </Heading>
