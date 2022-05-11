e5b44e4eecc58f886c96dc7a30de772dddbe7be8
Anselmo Ichihara <ichihara7l@gmail.com>
2022-05-10 22:59:14 -0300

    Ajust layout invite

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { Box, Image, Flex, Badge, Text } from "@chakra-ui/react";
 import { MdStar } from "react-icons/md";
 import { GiGreekTemple } from 'react-icons/gi'
 import styled from '@emotion/styled';
+import Head from 'next/head';
 
 const Caixa = styled(Flex)`
   background-image: url(/images/a.jpg);
@@ -13,31 +14,54 @@ const Caixa = styled(Flex)`
 
 function Home() {
   return (
-    <Caixa h="100vh" flexDirection="column" justify="flex-end">
-      <Box background="white" p="5" w="full" h="12rem" borderWidth="1px" opacity="0.6">
-        <Text mt={2} fontSize="3xl" fontWeight="semibold" lineHeight="short">
-          Super casamento Margo e Anselmo
-        </Text>
-        <Flex mt={2} align="center">
-          <Box as={MdStar} color="orange.400" />
-          <Text ml={1} fontSize="sm">
-            <b>18/08/2022</b> 
+    <div>
+      <Head>
+      {/* <!-- Primary Meta Tags --> */}
+        <title>Nosso convite de casamento</title>
+        <meta name="title" content="Nosso convite de casamento" />
+        <meta name="description" content="Muito importante a confirmação de sua presença." />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website"/> 
+        <meta property="og:url" content="https://wedd-ichihara7l.vercel.app/guestInvitation/62766327ab987346e0f88fe3"/>
+        <meta property="og:title" content="Nosso convite de casamento"/>
+        <meta property="og:description" content="Muito importante a confirmação de sua presença."/>
+        <meta property="og:image" content=""/>
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://wedd-ichihara7l.vercel.app/guestInvitation/62766327ab987346e0f88fe3"/>
+        <meta property="twitter:title" content="Nosso convite de casamento"/>
+        <meta property="twitter:description" content="Muito importante a confirmação de sua presença."/>
+        <meta property="twitter:image" content=""/>        
+      </Head>
+
+      <Caixa h="100vh" flexDirection="column" justify="flex-end">
+        <Box background="white" p="5" w="full" h="12rem" borderWidth="1px" opacity="0.6">
+          <Text mt={2} fontSize="3xl" fontWeight="semibold" lineHeight="short">
+            Super casamento Margo e Anselmo
           </Text>
-        </Flex>
-        <Flex mt={2} align="center">
-          <Box as={GiGreekTemple} color="orange.400" />
-          <Text ml={1} fontSize="sm">
-            <b>Celebração as 21:00 - Igreja de São Francisco - Sete Lagoas - Bairro JK</b> 
-          </Text>
-        </Flex>
-        <Flex mt={2} align="center">
-          <Box as={GiGreekTemple} color="orange.400" />
-          <Text ml={1} fontSize="sm">
-            <b>Recepção a partir das 22:00 - Sítio Primavera</b> 
-          </Text>
-        </Flex>
-      </Box>
-    </Caixa>
+          <Flex mt={2} align="center">
+            <Box as={MdStar} color="orange.400" />
+            <Text ml={1} fontSize="sm">
+              <b>18/08/2022</b> 
+            </Text>
+          </Flex>
+          <Flex mt={2} align="center">
+            <Box as={GiGreekTemple} color="orange.400" />
+            <Text ml={1} fontSize="sm">
+              <b>Celebração as 21:00 - Igreja de São Francisco - Sete Lagoas - Bairro JK</b> 
+            </Text>
+          </Flex>
+          <Flex mt={2} align="center">
+            <Box as={GiGreekTemple} color="orange.400" />
+            <Text ml={1} fontSize="sm">
+              <b>Recepção a partir das 22:00 - Sítio Primavera</b> 
+            </Text>
+          </Flex>
+        </Box>
+      </Caixa>
+    </div>
   );
 
 } 
