280f0f96f01c982530a1237969ae0b91d9683056
Pedro Henrique Ribeiro Moreira <overlord@pop-os.localdomain>
2022-05-14 20:53:07 -0300

    Link do google maps

--- src/components/Locate.jsx
+++ src/components/Locate.jsx
@@ -2,8 +2,8 @@ import { useEffect,  useState } from 'react';
 import { Heading, Box, Center, Text, Button,
   useColorModeValue, FormControl} from '@chakra-ui/react';
 import styled from '@emotion/styled';
-import { BellIcon, SunIcon } from '@chakra-ui/icons'
-
+import { SunIcon } from '@chakra-ui/icons'
+import { FaChurch } from 'react-icons/fa';
 
 const Fundo = styled(Center)`
   position:absolute;
@@ -33,13 +33,14 @@ export default function Locate({Visivel}) {
           p={6}
           textAlign={'center'}>
           <Heading fontSize={'2xl'} fontFamily={'body'} color={'#5f5694 '}>
-            <BellIcon />            
+            <FaChurch />
             <br />
             Igreja
           </Heading>
           <Text fontWeight={600} color={'gray.500'} mb={4}>
             R. Itaipú, 53 - São Francisco de Assis, Sete Lagoas - MG
           </Text>
+          <a target="_blank" href="https://www.google.com.br/maps/place/Igreja+S%C3%A3o+Francisco+de+Assis/@-19.4299745,-44.2496829,17z/data=!3m1!4b1!4m5!3m4!1s0xa645586f98be91:0x7e87d2481e6b4c82!8m2!3d-19.4300211!4d-44.2474724?hl=pt-BR&authuser=0">Ver rota</a>
           <br />
           <br />
           <br />
