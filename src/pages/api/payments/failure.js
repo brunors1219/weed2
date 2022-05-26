035fb51662accae6d1c00803e8812308e6c798a5
Anselmo Ichihara <ichihara7l@gmail.com>
2022-05-25 22:11:12 -0300

    Testing result payment 15

--- src/pages/api/payments/failure.js
+++ src/pages/api/payments/failure.js
@@ -11,6 +11,8 @@ export default async function handler(request, response) {
     Pagamento.excluded = true;
     Pagamento.save();
 
+    return response.status(500).json({ message: JSON.stringify(request.query) });
+
     return response.redirect(`${process.env.APP_URL}/guestInvitationv3/${Pagamento.guest}`);
   }else{
     return response.status(500).json({ message: 'Registro não encontrado para atualização' });
