748329b0f247c8d714712eb755edbcb664c60a59
Anselmo Ichihara <ichihara7l@gmail.com>
2022-05-28 18:47:33 -0300

    Added route with maps

--- src/components/Locate.jsx
+++ src/components/Locate.jsx
@@ -3,7 +3,7 @@ import { Heading, Box, Center, Text, Button,
   useColorModeValue, FormControl} from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { SunIcon } from '@chakra-ui/icons'
-import { FaChurch } from 'react-icons/fa';
+import { FaChurch, FaRoute } from 'react-icons/fa';
 
 const Fundo = styled(Center)`
   position:absolute;
@@ -32,17 +32,25 @@ export default function Locate({Visivel}) {
           rounded={'lg'}
           p={6}
           textAlign={'center'}>
-          <Heading fontSize={'2xl'} fontFamily={'body'} color={'#5f5694 '}>
-            <FaChurch />
-            <br />
+
+          <Heading textAlign={'center'} fontSize={'2xl'} fontFamily={'body'} color={'#5f5694 '}>
+            <Center>
+              <FaChurch />
+            </Center>            
             Igreja
           </Heading>
           <Text fontWeight={600} color={'gray.500'} mb={4}>
             R. Itaipú, 53 - São Francisco de Assis, Sete Lagoas - MG
           </Text>
-          <a target="_blank" href="https://www.google.com.br/maps/place/Igreja+S%C3%A3o+Francisco+de+Assis/@-19.4299745,-44.2496829,17z/data=!3m1!4b1!4m5!3m4!1s0xa645586f98be91:0x7e87d2481e6b4c82!8m2!3d-19.4300211!4d-44.2474724?hl=pt-BR&authuser=0">Ver rota</a>
-          <br />
-          <br />
+          <a 
+            target="_blank"             
+            href="https://www.google.com.br/maps/place/Igreja+S%C3%A3o+Francisco+de+Assis/@-19.4299745,-44.2496829,17z/data=!3m1!4b1!4m5!3m4!1s0xa645586f98be91:0x7e87d2481e6b4c82!8m2!3d-19.4300211!4d-44.2474724?hl=pt-BR&authuser=0">
+            <Center>
+              <FaRoute 
+                color={"#64a283"}
+                fontSize={"xxx-large"}/>
+            </Center>
+          </a>
           <br />
           <Heading fontSize={'2xl'} fontFamily={'body'} color={'#5f5694 '}>
             <SunIcon />
@@ -52,7 +60,17 @@ export default function Locate({Visivel}) {
           <Text fontWeight={600} color={'gray.500'} mb={4}>
             Avenida Arquimedes, 145 - JK, Sete Lagoas - MG
           </Text>
+          <a 
+            target="_blank"             
+            href="https://www.google.com/maps/place/Av.+Arquimedes,+145+-+Jk,+Sete+Lagoas+-+MG,+35702-240/@-19.4190029,-44.2466727,17z/data=!4m13!1m7!3m6!1s0xa64566a11e38fd:0xa9647136db3c149b!2sAv.+Arquimedes,+145+-+Jk,+Sete+Lagoas+-+MG,+35702-240!3b1!8m2!3d-19.419003!4d-44.242188!3m4!1s0xa64566a11e38fd:0xa9647136db3c149b!8m2!3d-19.419003!4d-44.242188">
+            <Center>
+              <FaRoute 
+                color={"#64a283"}
+                fontSize={"xxx-large"}/>
+            </Center>
+          </a>
           <br />
+
           <FormControl id="name" flexDirection={'column'} p={1}>
             <Button
               variant="solid"
